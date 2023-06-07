ecaeafcec8c3dead3e6cf26a56f0d053f5f5d068
etaylor5 <etaylorsubs1993@gmail.com>
2023-06-06 22:07:30 -0400

    + Added source files for Section 2: Variables

---
+++ Java-Bootcamp/Section-2-Variables/Decimals.java
@@ -0,0 +1,11 @@
+public class Decimals {
+    public static void main(String[] args) {
+        double percentage = 73.5;
+        System.out.println(percentage + " of percentages are made up.");
+
+        // Change double to int to see why we never use int for mathematical calculations.
+        double wallet = 20;
+        int people = 3;
+        System.out.println(wallet / people);
+    }
+}
---
+++ Java-Bootcamp/Section-2-Variables/Workbook-2.2/Biography.java
@@ -0,0 +1,17 @@
+public class Biography {
+    public static void main(String[] args) {
+        String name = "Emmanuel";
+        int age = 20;
+        String country = "Grenada";
+        String sport = "basketball";
+        int hours = 2;
+        String game = "Pokemon";
+        String subject = "Math";
+        char grade = 'A';
+     
+        System.out.println("My name is " + name + ". I'm " + age + " years old, and I'm from " + country + ".");
+        System.out.println("My favourite sport is " + sport + ". I play for " + hours + " hour(s) a day");
+        System.out.println("When I'm tired, I like to play " + game + ".");
+        System.out.println("In school, my favourite subject was " + subject + ", I scored a " + grade + ".");
+    }
+}
