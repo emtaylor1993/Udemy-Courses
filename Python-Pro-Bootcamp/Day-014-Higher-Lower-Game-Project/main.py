9a4bcab12e156170959a8d30b0fa9510d5669b94
etaylor5 <etaylorsubs1993@gmail.com>
2023-07-22 11:19:28 -0400

    + Added source filse for Day 14: Higher Lower Game

---
+++ Python-Pro-Bootcamp/Day-014-Higher-Lower-Game-Project/main.py
@@ -0,0 +1,31 @@
+from art import logo, vs
+from game_data import data
+from replit import clear
+from random import choice
+
+incorrect = "n"
+score = 0
+option_a = choice(data)
+option_b = choice(data)
+
+while incorrect == "n":
+  clear()
+  print(logo)
+
+  if score > 0:
+    print(f"You're right! Current score: {score}.")
+    option_a = option_b
+    while option_a == option_b:
+      option_b = choice(data)
+  print(f"Compare A: {option_a['name']}, a {option_a['description']}, from {option_a['country']}")
+  print(vs)
+  print(f"Against B: {option_b['name']}, a {option_b['description']}, from {option_b['country']}")
+  answer = "A" if option_a["follower_count"] > option_b["follower_count"] else "B"
+  guess = input("Who has more followers? Type 'A' or 'B': ")
+  if guess == answer:
+    score = score + 1
+  else:
+    incorrect = "y"
+    clear()
+    print(logo)
+    print(f"Sorry, that's wrong. Final score: {score}")
