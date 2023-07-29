595964b61ba46a8cfef3d3ddf88e29028e57f4e1
etaylor5 <etaylorsubs1993@gmail.com>
2023-07-29 19:46:53 -0400

    + Added source files for Day 32: Sending Emails and Managing Dates

---
+++ Python-Pro-Bootcamp/Day-032-Send-Email-and-Managing-Dates/Monday-Motivation/main.py
@@ -0,0 +1,21 @@
+import datetime as dt
+import smtplib
+from random import choice
+
+now = dt.datetime.now()
+day = now.strftime("%A")
+my_email = "python.class.testing@gmail.com"
+password = "rcwqwajctcbcqiuh"
+
+with open("quotes.txt", "r") as f:
+    lines = f.readlines()
+
+if day == "Monday":
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(
+            from_addr=my_email, 
+            to_addrs="python.classtesting@yahoo.com", 
+            msg=f"Subject:Motivation\n\n{choice(lines)}."
+        )
